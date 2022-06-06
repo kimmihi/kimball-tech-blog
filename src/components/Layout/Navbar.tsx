32faa0f11bfbeb094a57d9804f88b84eb071c479
kimmeh <mhko0411@gmail.com>
2022-06-06 15:28:05 +0900

    change blog name for check ci/cd

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar: NextPage = () => {
               fontSize: "24px",
             }}
           >
-            {"Kimball's"}
+            {"Kim"}
           </Typography>
         </a>
       </Link>
